78483e18d487fb15b0033202a0fad0ab1c98bbbd
Khalid Saafan <khalidsafan860@gmail.com>
2026-04-23 00:04:43 +0200

    Added fast React Pizza Co. final Project

---
+++ src/features/cart/CartOverview.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getTotalCartQuantity, getTotalCartPrice } from "./CartSlice";
+import { formatCurrency } from "./../../utils/helpers";
+
+function CartOverview() {
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+
+  if (!totalCartQuantity) return null;
+  return (
+    <div className="flex items-center justify-between bg-stone-800 p-4 uppercase text-stone-200 sm:px-6">
+      <p className="space-x-4 text-sm font-semibold text-stone-300 sm:space-x-6 md:text-base">
+        <span>{totalCartQuantity} pizzas</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
+      </p>
+
+      <Link to="/cart">Open cart &rarr;</Link>
+      {/* <a href="#">Open cart &rarr;</a> */}
+    </div>
+  );
+}
+
+export default CartOverview;
---
+++ src/features/menu/MenuItem.jsx
@@ -0,0 +1,66 @@
+import { useDispatch, useSelector } from "react-redux";
+import { formatCurrency } from "../../utils/helpers";
+import Button from "./../../ui/Button";
+import { addItem, getCurrentQuantityById } from "../cart/CartSlice";
+import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "./../cart/UpdateItemQuantity";
+
+function MenuItem({ pizza }) {
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
+  return (
+    <li className="flex gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`${soldOut ? "opacity-70 grayscale" : ""} h-24`}
+      />
+      <div className="flex grow flex-col pt-0.5">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex items-center justify-between">
+          {!soldOut ? (
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="text-sm font-medium uppercase text-stone-500">
+              Sold out
+            </p>
+          )}
+
+          <div className="flex items-center gap-4">
+            {isInCart && (
+              <>
+                <UpdateItemQuantity
+                  pizzaId={id}
+                  currentQuantity={currentQuantity}
+                />
+                <DeleteItem pizzaId={id} />
+              </>
+            )}
+            {!soldOut && !isInCart && (
+              <Button onClick={handleAddToCart} type="small">
+                Add to cart
+              </Button>
+            )}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default MenuItem;
